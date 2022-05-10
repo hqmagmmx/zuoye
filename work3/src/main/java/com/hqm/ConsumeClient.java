d3aabde65fa6d941b40f95768cf35bc58790b7a5
hqmagmmx <1584342341@qq.com>
2022-05-10 09:54:25 +0800

    作业五

---
+++ work3/src/main/java/com/hqm/ConsumeClient.java
@@ -0,0 +1,11 @@
+package com.hqm;
+
+import java.io.IOException;
+
+public class ConsumeClient {
+    public static void main(String[] args) throws IOException {
+        MqClient mqClient = new MqClient();
+        String message = mqClient.consume();
+        System.out.println("获取消息为："+message);
+    }
+}
